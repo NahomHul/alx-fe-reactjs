50305daf8949362f045aab767046414873bbb483
Nahom Hul <nahomhulum@gmail.com>
2025-08-30 11:28:34 +0300

    import new permissions

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,14 @@
-import './App.css'
-import WelcomeMessage from './components/WelomeMessage.jsx'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
     <div>
+      <Header />
+      <MainContent />
+      <Footer />
       <WelcomeMessage />
     </div>
   );
