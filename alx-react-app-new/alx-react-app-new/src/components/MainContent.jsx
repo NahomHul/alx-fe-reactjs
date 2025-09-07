265de8890e24447b6dcd26f02c2a6ef9c0e0214f
Nahom Hul <nahomhulum@gmail.com>
2025-09-07 14:20:33 +0300

    remove files

--- alx-react-app-new/alx-react-app-new/src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent() {
-  return (
-    <main style={{ padding: '20px', backgroundColor: '#f4f4f4', minHeight: '200px' }}>
-      <p>Explore the list of my favorite cities with details below.</p>
-    </main>
-  );
-}
-
-export default MainContent;
