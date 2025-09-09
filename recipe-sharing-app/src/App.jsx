b264b910bd517cc5faf5009fcc1514ebb31e9408
Nahom Hul <nahomhulum@gmail.com>
2025-09-09 11:02:19 +0300

    fixing App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
+import { useRecipeStore } from './store/recipeStore';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
+  const generateRecommendations = useRecipeStore(
+    (state) => state.generateRecommendations
+  );
+
   return (
-    <div>
+    <BrowserRouter>
       <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+      <Routes>
+        {}
+        <Route
+          path="/"
+          element={
+            <div>
+              <SearchBar />
+              <AddRecipeForm />
+              <RecipeList />
+              <FavoritesList />
+              <button onClick={generateRecommendations}>
+                Get Recommendations
+              </button>
+              <RecommendationsList />
+            </div>
+          }
+        />
+
+        {/* ✅ details page */}
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
