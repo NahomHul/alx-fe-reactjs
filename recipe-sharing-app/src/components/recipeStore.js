a90eb39c7f6ca764727da12c117d13fd65f084dc
Nahom Hul <nahomhulum@gmail.com>
2025-09-09 10:37:09 +0300

    update & delete functionality

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,5 +4,12 @@ export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+  updateRecipe: (updated) =>
+    set((state) => ({
+      recipes: state.recipes.map((r) => (r.id === updated.id ? updated : r)),
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
+    })),
 }));
