814fec12a5c9d8111d07901b2a9acc4e3706b09b
Nahom Hul <nahomhulum@gmail.com>
2025-09-19 14:10:52 +0300

    setup styling

--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -8,3 +8,14 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+// src/main.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
