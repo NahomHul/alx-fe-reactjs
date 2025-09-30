0d439b3e95156b1839fad2ca44975aa5ffc171a0
Nahom Hul <nahomhulum@gmail.com>
2025-09-30 10:33:40 +0300

    modify app.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,7 @@
+import HomePage from "./components/HomePage";
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold text-blue-500 text-center mt-10">
-      Recipe Sharing Platform 
-    </h1>
-  );
+  return <HomePage />;
 }
 
 export default App;
