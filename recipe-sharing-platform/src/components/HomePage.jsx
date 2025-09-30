8ed9e21da40a213df51921146a681138772ca116
Nahom Hul <nahomhulum@gmail.com>
2025-09-30 10:42:38 +0300

    add link

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,6 +12,13 @@ function HomePage() {
 
   return (
     <div className="p-6">
+        <a
+            href="/add-recipe"
+            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mb-4 inline-block"
+            >
+            ➕ Add Recipe
+        </a>
+
       <h1 className="text-4xl font-bold text-center mb-8">🍽️ Recipe Sharing Platform</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
