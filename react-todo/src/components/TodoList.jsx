93c6fdf75fdc4622a2b041b31dfa76c8da5a50c8
Nahom Hul <nahomhulum@gmail.com>
2025-10-07 14:28:27 +0300

    todo list component

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -9,34 +9,44 @@ export default function TodoList() {
 
   const addTodo = (text) => {
     const newTodo = { id: Date.now(), text, completed: false };
-    setTodos([...todos, newTodo]);
+    setTodos((prev) => [...prev, newTodo]);
   };
 
   const toggleTodo = (id) => {
-    setTodos(
-      todos.map(todo =>
+    setTodos((prev) =>
+      prev.map((todo) =>
         todo.id === id ? { ...todo, completed: !todo.completed } : todo
       )
     );
   };
 
   const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
 
   return (
-    <div>
+    <div data-testid="todo-list">
       <h2>Todo List</h2>
       <AddTodoForm addTodo={addTodo} />
       <ul>
-        {todos.map(todo => (
+        {todos.map((todo) => (
           <li
             key={todo.id}
             onClick={() => toggleTodo(todo.id)}
-            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+            style={{
+              textDecoration: todo.completed ? "line-through" : "none",
+              cursor: "pointer",
+            }}
           >
             {todo.text}
-            <button onClick={(e) => { e.stopPropagation(); deleteTodo(todo.id); }}>Delete</button>
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                deleteTodo(todo.id);
+              }}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
