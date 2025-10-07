fda50f47c7200f3170344051c466b20448f40c1b
Nahom Hul <nahomhulum@gmail.com>
2025-10-07 14:29:10 +0300

    add todo

--- react-todo/src/components/AddTodoForm.jsx
+++ react-todo/src/components/AddTodoForm.jsx
@@ -13,9 +13,10 @@ export default function AddTodoForm({ addTodo }) {
   return (
     <form onSubmit={handleSubmit}>
       <input
+        type="text"
+        placeholder="Add new todo"
         value={input}
         onChange={(e) => setInput(e.target.value)}
-        placeholder="Add new todo"
       />
       <button type="submit">Add</button>
     </form>
