801cefb5f7ce8e11f1be456755d5d1aaa864fa7d
Nahom Hul <nahomhulum@gmail.com>
2025-10-07 15:01:14 +0300

    jest config

--- react-todo/jest.config.js
+++ react-todo/jest.config.js
@@ -1,7 +1,15 @@
 export default {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
+    '^.+\\.jsx?$': 'babel-jest',
   },
-  moduleFileExtensions: ["js", "jsx"],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // If using aliases; optional
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.d.ts',
+  ],
 };
